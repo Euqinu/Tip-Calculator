6edd6b0f4253ec2137e09510bbc4bbdd29ad3d78
Unique Chhetri <uniquechhetrii@gmail.com>
2018-03-17 00:30:30 +0530

    Add files via upload

---
+++ js/tipScript.js
@@ -0,0 +1,36 @@
+// hide the total amount on load
+
+function calculateTip(){
+	var billAmount= document.getElementById("billAmount").value;
+	var serviceQuality= document.getElementById("serviceQuality").value;
+	var totalPeople= document.getElementById("totalPeople").value;
+
+	//...........validation.............
+	if(billAmount==="" || serviceQuality==0){
+		window.alert("Please put valid values");
+		return;
+	}
+	if(totalPeople===""|| totalPeople<=1){
+		totalPeople=1;
+
+		document.getElementById("each").style.display="none";
+	}
+	else{
+		document.getElementById("each").style.display="block";
+	}
+
+	var total=(billAmount*serviceQuality)/totalPeople;
+	total=Math.round(total*100)/100;
+	//total=total.toFixed(2);
+
+	//Display the tip
+	document.getElementById("totalTip").style.display="block";
+	document.getElementById("tip").innerHTML=total;
+}
+
+
+
+document.getElementById("totalTip").style.display="none";
+//document.getElementById("each").style.display="none";
+
+document.getElementById('calculate').onclick=function(){ calculateTip();};
